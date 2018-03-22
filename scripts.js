03f2bae416613a11e372a9aac1d8d0084f94e79f
jeremy-mutitu <jeremiahmutitu@gmail.com>
2018-03-22 11:36:01 +0300

    add all files

---
+++ scripts.js
@@ -0,0 +1,17 @@
+//  $(document).ready(function() {
+  //  $("p").click(function() {
+    //  $("img").show();
+  //  });
+//  });
+//  $(document).ready(function() {
+//    $("p").click(function() {
+  //    $(".walrus-showing").show();
+    //  $(".walrus-hidden").hide();
+    //});
+//  });
+$(document).ready(function() {
+     $(".clickable").click(function() {
+       $("#initially-showing").toggle();
+       $("#initially-hidden").toggle();
+     });
+   });
